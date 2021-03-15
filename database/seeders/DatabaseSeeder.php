22f8bbe941a6726df41e92d8be855d0de39d156d
Muhammad Khalaf <mohammad.samir.khalaf@gmail.com>
2021-03-16 01:18:33 +0200

    sssss

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        \App\Models\Clinic::factory(20)->create();
-        \App\Models\Major::factory(1)->create();
-        \App\Models\User::factory(1)->create();
+//        \App\Models\Major::factory(1)->create();
+//        \App\Models\User::factory(1)->create();
 //        \App\Models\ClinicMajor::factory(10)->create();
     }
 }
